60297572dff9f11417d22ce16466de5baf91066e
jorge <jorge.fernandez@dmsti.es>
2008-12-04 12:56:10 +0100

    Fixed rake scripts for DB migration

--- db/migrate0/003_rename_user_to_files.rb
+++
@@ -1,8 +0,0 @@
-class RenameUserToFiles < ActiveRecord::Migration
-  def self.up
-    rename_column :files, :author_id, :user_id
-  end
-
-  def self.down
-  end
-end
--- db/migrate0/004_add_state_to_file.rb
+++
@@ -1,9 +0,0 @@
-class AddStateToFile < ActiveRecord::Migration
-  def self.up
-    add_column :files, :state, :string
-  end
-
-  def self.down
-    remove_column :files, :state
-  end
-end
